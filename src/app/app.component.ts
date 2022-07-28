274e368128ad32f59aed589b3f33f345da7bd665
Alumine Meza <Alumine Meza>
2022-07-28 12:49:53 -0300

    primer commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,39 @@
 import { Component } from '@angular/core';
 
 @Component({
-  selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  selector: `app-root`,
+  templateUrl: `app.component.html`
 })
+
+
 export class AppComponent {
-  title = 'bases';
+  
 }
+
+ 
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+ // sumar() {
+  //   this.numero += 1;
+  // }
+
+  // restar() {
+  //   this.numero -= 1;
+  // }
